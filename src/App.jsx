3913223c7bd5b83182c7699065942ec00201d858
Alexis Tercero <alexistercero55@gmail.com>
2023-03-12 18:41:09 -0600

    TASK | Basic Vite-React-JS templete uwu.
    Signed-off-by: Alexis Tercero <alexistercero55@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,9 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
+    <main className="App">
+
+      <div className='logos'>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
@@ -16,18 +17,14 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+      
+      <button onClick={() => setCount((count) => count + 1)}>
+        count is {count}
+      </button>
+
+      <div className='URI'>Templete by Alexis Tercero</div>
+
+    </main>
   )
 }
 
