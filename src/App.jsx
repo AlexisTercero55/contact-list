519890dfe2dbb8c1bf00d9f2a088258eecc4b621
Alexis Tercero <alexistercero55@gmail.com>
2023-03-12 18:57:31 -0600

    RELEASE | Vite-React-JS uwu
    Signed-off-by: Alexis Tercero <alexistercero55@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 
       <div className='logos'>
         <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
+          <img src="./vite.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
