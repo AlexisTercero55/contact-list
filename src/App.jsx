ba3c9895fb51bc38684da8a40f7151ada32d8242
Alexis Tercero <alexistercero55@gmail.com>
2023-03-22 14:23:24 -0600

    TASK | New contact form.
    Signed-off-by: Alexis Tercero <alexistercero55@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,125 @@
  * @mail    alexistercero55@gmail.com
  * @license MPL-2.0
  */
-import { useState, useContext  } from 'react';
+import { useState  } from 'react';
 import { ProfileContextProvider } from './context/ProfileContext';
 import './App.css'
 import Container from 'react-bootstrap/Container';
 import ContactList from './components/ContactList';
+import { Button } from 'react-bootstrap';
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
 
 function App() {
+
+  const [show, setShow] = useState(false);
+  const [name, setname] = useState('');
+  const [position, setPosition] = useState('');
+  const [mail, setMail] = useState('');
+  const [projectID, setprojectID] = useState('');
+  const handleClose = (e) => {
+    if(e)
+    {
+      e?.preventDefault();
+
+      
+
+      console.log(name);
+      console.log(position);
+      console.log(mail);
+      console.log(projectID);
+    }
+    
+    setShow(false);
+  }
+  const handleShow = () => setShow(true);
+
   return (
     <Container fluid >
     <main className="App">
       <header className='my-3'>
         <h1>Contact list</h1>
       </header>
+      {/* new profile modal form */}
+      <Button onClick={handleShow}>
+        New contact
+      </Button>
+      
       <ProfileContextProvider>
         <ContactList ></ContactList>
       </ProfileContextProvider>
     </main>
+
+
+    <Modal 
+    centered
+    show={show} 
+    onHide={handleClose}
+    >
+      <Modal.Header closeButton>
+        <Modal.Title>New contact info</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        {/* New contact form */}
+        <Form onSubmit={handleClose}>
+
+          <Form.Group className="mb-3" controlId='name'>
+            <Form.Label>Name</Form.Label>
+            <Form.Control 
+            value={name} onChange={(e)=>setname(e.target.value)}
+            type="text" placeholder="Enter user name" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId='position'>
+            <Form.Label>Position</Form.Label>
+            <Form.Control 
+            value={position} onChange={(e)=>setPosition(e.target.value)}
+            type="text" placeholder="Enter user role" />
+          </Form.Group>
+        
+          <Form.Group className="mb-3" controlId="email">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control 
+            value={mail} onChange={(e)=>setMail(e.target.value)}
+            type="email" placeholder="Enter email"
+             />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId='projectID'>
+            <Form.Label>Project number</Form.Label>
+            <Form.Control
+            value={projectID} onChange={(e)=>setprojectID(e.target.value)}
+            style={{width:'8em'}} 
+            size='sm'
+            type="number" placeholder={0} />
+          </Form.Group>
+          <Form.Group>
+          {/* <Button variant="secondary" onClick={handleClose}>
+          Cancel
+          </Button> */}
+          <Button variant="primary" 
+                  type='submit'>
+            Save contact
+          </Button>
+          </Form.Group>
+        </Form>
+        
+        
+      </Modal.Body>
+      {/* <Modal.Footer>
+        <Button variant="secondary" onClick={handleClose}>
+          Cancel
+        </Button>
+        <Button variant="primary" onClick={handleClose}>
+          Save contact
+        </Button>
+      </Modal.Footer> */}
+    </Modal>
+
+
     </Container>
   )
 }
