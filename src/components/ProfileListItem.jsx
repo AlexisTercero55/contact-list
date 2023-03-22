052960930e7c171f3c763e60cb88d6f68000b626
Alexis Tercero <alexistercero55@gmail.com>
2023-03-21 23:47:12 -0600

    BUG | deleteProfile action - confirmation process.
    Signed-off-by: Alexis Tercero <alexistercero55@gmail.com>

--- src/components/ProfileListItem.jsx
+++ src/components/ProfileListItem.jsx
@@ -4,25 +4,16 @@
  * @mail    alexistercero55@gmail.com
  * @license MPL-2.0
  */
-import { useContext } from 'react';
+import { useContext, useState  } from 'react';
 import { ProfileContext, 
-         ProfileActions,
-         } from '../context/ProfileContext';
+         ProfileActions } from '../context/ProfileContext';
 import Image from 'react-bootstrap/Image';
 import Badge from 'react-bootstrap/Badge';
+import Modal from 'react-bootstrap/Modal';
 import { Button, Dropdown, DropdownButton } from 'react-bootstrap';
 import { BsFillPenFill, BsTrash, BsThreeDotsVertical } from 'react-icons/bs';
 
-/** //! needs typed support
- * ProfileListItem is just a view of 
- * Profile interface (any contact)
- * {
- *  name,
- *  email,
- *  attributes...
- *  
- * }
- */
+
 const ProfileListItem = ({
   index,
   avatarUrl,
@@ -31,108 +22,137 @@ const ProfileListItem = ({
   email = 'alexistercero55@gmail.com',
   projectID = 100
 }) => {
-
-  // console.log(index);
   const  [PROFILES, dispatch] = useContext(ProfileContext);
+  // modal logic : delete profile confirmation
+  const [show, setShow] = useState(false);
+  const handleShow = () => setShow(true);
+  const deleteProfileConfirmation = (confirmation=false) => 
+  {
+    setShow(false);
+    if(confirmation)
+      dispatch({type:ProfileActions.deleteProfile, index});
+  };
 
-
-
-  
-    return (
-<tr className='ProfileRow'>
-    <th scope="row" className="ps-4">
-        <div className="form-check my-2">
-          <input type="checkbox" 
-            className="form-check-input" 
-            id="contacusercheck1" />
-          <label className="form-check-label" 
-                htmlFor="contacusercheck1">
-          </label>
+  return (
+  <>
+  <tr className='ProfileRow'>
+      <th scope="row" className="ps-4">
+          <div className="form-check my-2">
+            <input type="checkbox" 
+              className="form-check-input" 
+              id="contacusercheck1" />
+            <label className="form-check-label" 
+                  htmlFor="contacusercheck1">
+            </label>
+          </div>
+      </th>
+      <td className='image-profileName'>
+        <div className='
+          d-flex 
+          align-items-center
+          px-2 my-2'>
+          <Image roundedCircle
+          src={avatarUrl}
+          width="25px"
+          className='me-2'
+          ></Image>
+          <span >
+            {name}
+          </span>
         </div>
-    </th>
-    <td className='image-profileName'>
-      <div className='
-        d-flex 
-        align-items-center
-        px-2 my-2'>
-        <Image roundedCircle
-        src={avatarUrl}
-        width="25px"
-        className='me-2'
-        ></Image>
-        <span >
-          {name}
-        </span>
+      </td>
+      <td className='role'>
+      <div className='d-flex 
+          align-items-center 
+          my-2
+          px-2
+          '>
+        <Badge bg="light" className='badge-soft-success'>
+          {position}
+        </Badge>
       </div>
-    </td>
-    <td className='role'>
-    <div className='d-flex 
-        align-items-center 
-        my-2
-        px-2
-        '>
-      <Badge bg="light" className='badge-soft-success'>
-        {position}
-      </Badge>
-    </div>
-    </td>
-    <td className='mail'>
-    <div className='d-flex 
-        align-items-center 
-        my-2 px-2
-        '>
-        {email}
-    </div>
-    </td>
-    <td className='project-number'>
-    <div className='d-flex 
-        align-items-center 
-        justify-content-center
-        my-2
-        '>
-        {projectID}
+      </td>
+      <td className='mail'>
+      <div className='d-flex 
+          align-items-center 
+          my-2 px-2
+          '>
+          {email}
       </div>
-    </td>
-    <td className='profile-options'>
-    <ul className="list-inline mb-0 d-flex flex-row">
-      <li className="list-inline-item">
-        <Button variant="link" //needs onclick action
-                data-bs-toggle="tooltip" 
-                data-bs-placement="top" 
-                title="Edit" 
-                className="text-primary"
-                onClick={()=> dispatch({type:ProfileActions.changeProject, index})}>
-          <BsFillPenFill size={18} />
-        </Button>
-      </li>
-      <li className="list-inline-item">
-        <Button variant="link" //needs onclick action
-                data-bs-toggle="tooltip" 
-                data-bs-placement="top" 
-                title="Delete" 
-                className="text-danger"
-                onClick={()=> dispatch({type:ProfileActions.deleteProfile, index})}>
-          <BsTrash size={18} />
-        </Button>
-      </li>
-      <li className="list-inline-item dropdown">
-        <DropdownButton id="dropdown-basic-button" 
-          title={<BsThreeDotsVertical size={18} />} 
-          variant="link" 
-          className="font-size-18 "
-          drop="down">
-          <Dropdown.Item href="#"//needs onclick action
-          >Action</Dropdown.Item>
-          <Dropdown.Item href="#">Another action</Dropdown.Item>
-          <Dropdown.Item href="#">Something else here</Dropdown.Item>
-        </DropdownButton>
-      </li>
-    </ul>
-    </td> 
-</tr>
-    );
-}
+      </td>
+      <td className='project-number'>
+      <div className='d-flex 
+          align-items-center 
+          justify-content-center
+          my-2
+          '>
+          {projectID}
+        </div>
+      </td>
+      <td className='profile-options'>
+      <ul className="list-inline mb-0 d-flex flex-row">
+        <li className="list-inline-item">
+          <Button variant="link" //needs onclick action
+                  data-bs-toggle="tooltip" 
+                  data-bs-placement="top" 
+                  title="Edit" 
+                  className="text-primary"
+                  onClick={()=> dispatch({type:ProfileActions.changeProject, index})}>
+            <BsFillPenFill size={18} />
+          </Button>
+        </li>
+        <li className="list-inline-item">
+          <Button variant="link" //needs onclick action
+                  data-bs-toggle="tooltip" 
+                  data-bs-placement="top" 
+                  title="Delete" 
+                  className="text-danger"
+                  onClick={handleShow}>
+            <BsTrash size={18} />
+          </Button>
+        </li>
+        <li className="list-inline-item dropdown">
+          <DropdownButton id="dropdown-basic-button" 
+            title={<BsThreeDotsVertical size={18} />} 
+            variant="link" 
+            className="font-size-18 "
+            drop="down">
+            <Dropdown.Item href="#"//needs onclick action
+            >Action</Dropdown.Item>
+            <Dropdown.Item href="#">Another action</Dropdown.Item>
+            <Dropdown.Item href="#">Something else here</Dropdown.Item>
+          </DropdownButton>
+        </li>
+      </ul>
+      </td> 
+  </tr>
 
+  <Modal 
+  show={show} 
+  onHide={deleteProfileConfirmation}
+  centered>
+    <Modal.Header closeButton>
+      <Modal.Title>Are you sure to delete this profile?</Modal.Title>
+    </Modal.Header>
+    
+    <Modal.Body>User {name} will be deleted</Modal.Body>
+
+    <Modal.Footer>
+      <Button variant="danger" onClick={()=>{
+        deleteProfileConfirmation(true);
+      }}>
+        Delete
+      </Button>
+      <Button variant="primary" onClick={()=>{
+        deleteProfileConfirmation(false);
+      }}>
+        Cancel
+      </Button>
+    </Modal.Footer>
+  </Modal>
+  </>
+  );
+}
 export default ProfileListItem;
 {/* <tr className='ProfileRow-Component'>
     <th scope="row" className="ps-4">
